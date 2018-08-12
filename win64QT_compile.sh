1ba43b48e57fab9fc31460c48cdb133a61269e40
nashsclay <nashsclay@gmail.com>
2018-08-11 22:29:13 -0700

    Create win64QT_compile.sh

---
+++ win64QT_compile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+sudo make clean
+chmod 777 -R *
+sudo apt-get update
+sudo apt-get -y upgrade
+PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
+cd `pwd`/depends
+sudo make -j2 HOST=x86_64-w64-mingw32
+cd ..
+sudo ./autogen.sh
+mkdir db4
+wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+tar -xzvf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix/
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=/usr/local/bin/`pwd`/db4
+sudo make install
+cd ../../
+sudo ./autogen.sh
+./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32
+sudo make -j2
+echo "Remember to strip the QT file!"
