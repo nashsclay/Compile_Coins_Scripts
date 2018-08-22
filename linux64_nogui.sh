16ccba7d6fed2c1d11c3bc9db77362fc6bd3723d
nashsclay <nashsclay@gmail.com>
2018-08-22 00:14:22 -0700

    Update linux64_nogui.sh

--- linux64_nogui.sh
+++ linux64_nogui.sh
@@ -11,7 +11,7 @@ mkdir `pwd`/db4
 wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 tar -xzvf db-4.8.30.NC.tar.gz
 cd `pwd`/db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=/usr/local/bin/`pwd`/db4
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
 sudo make install
 cd ../../
 sudo ./autogen.sh
