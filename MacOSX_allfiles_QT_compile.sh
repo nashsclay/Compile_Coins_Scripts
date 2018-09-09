59dffcaa87092e28cb594f653b3ac6cb83afb1f0
nashsclay <nashsclay@gmail.com>
2018-09-09 15:52:35 -0700

    Update MacOSX_allfiles_QT_compile.sh

--- MacOSX_allfiles_QT_compile.sh
+++ MacOSX_allfiles_QT_compile.sh
@@ -23,4 +23,5 @@ cd ../../
 sudo ./autogen.sh
 ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-apple-darwin11
 sudo make -j2
+sudo make deploy
 echo "No strip required for this file!"
