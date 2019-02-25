442ca5d329f09ef17e865ece9d6ab440ac95b059
nashsclay <nashsclay@gmail.com>
2019-02-24 22:32:02 -0700

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -1,21 +1,21 @@
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt install -y build-essential libtool autotools-dev automake pkg-config /
-  bsdmainutils curl git libboost-all-dev libssl-dev libboost-tools-dev libdb++-dev /
-  libevent-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools /
-  libprotobuf-dev protobuf-compiler libevent-dev libboost-all-dev libdb++-dev libssl-dev /
-  libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev /
+sudo apt install -y build-essential libtool autotools-dev automake pkg-config \
+  bsdmainutils curl git libboost-all-dev libssl-dev libboost-tools-dev libdb++-dev \
+  libevent-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools \
+  libprotobuf-dev protobuf-compiler libevent-dev libboost-all-dev libdb++-dev libssl-dev \
+  libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev \
   libboost-test-dev libboost-thread-dev lib32gcc-4.8-dev g++-arm-linux-gnueabihf gcc-aarch64-linux-gnu nsis libgmp3-dev
 sudo apt install -y g++-mingw-w64-x86-64
 sudo apt install -y software-properties-common
 sudo apt install -y g++-mingw-w64-i686 mingw-w64-i686-dev
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y curl librsvg2-bin libtiff-tools bsdmainutils cmake imagemagick libcap-dev /
+sudo apt-get install -y curl librsvg2-bin libtiff-tools bsdmainutils cmake imagemagick libcap-dev \
   libz-dev libbz2-dev python-setuptools
-sudo apt-get install -y curl g++-aarch64-linux-gnu g++-4.8-aarch64-linux-gnu gcc-4.8-aarch64-linux-gnu /
-  binutils-aarch64-linux-gnu g++-arm-linux-gnueabihf g++-4.8-arm-linux-gnueabihf gcc-4.8-arm-linux-gnueabihf /
-  binutils-arm-linux-gnueabihf g++-4.8-multilib gcc-4.8-multilib binutils-gold bsdmainutils /
+sudo apt-get install -y curl g++-aarch64-linux-gnu g++-4.8-aarch64-linux-gnu gcc-4.8-aarch64-linux-gnu \
+  binutils-aarch64-linux-gnu g++-arm-linux-gnueabihf g++-4.8-arm-linux-gnueabihf gcc-4.8-arm-linux-gnueabihf \
+  binutils-arm-linux-gnueabihf g++-4.8-multilib gcc-4.8-multilib binutils-gold bsdmainutils \
   g++-aarch64-linux-gnu binutils-aarch64-linux-gnu g++-aarch64-linux-gnu
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
