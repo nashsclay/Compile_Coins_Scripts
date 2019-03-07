ab8a6f1b10c2fdf933dea5feb4611787af7e0493
nashsclay <nashsclay@gmail.com>
2019-03-07 12:01:06 -0700

    Update win64QT_compile.sh

--- win64QT_compile.sh
+++ win64QT_compile.sh
@@ -9,7 +9,7 @@ cd ..
 sudo ./autogen.sh
 sudo mkdir db4
 sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-tar -xzvf db-4.8.30.NC.tar.gz
+sudo tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix/
 sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
 sudo make install
