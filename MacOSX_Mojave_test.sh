44730cb39b7890e2f0c2fa733b3a68e7c3d378e6
nashsclay <nashsclay@gmail.com>
2019-03-08 16:54:22 -0700

    Update MacOSX_Mojave_test.sh

--- MacOSX_Mojave_test.sh
+++ MacOSX_Mojave_test.sh
@@ -3,23 +3,23 @@ sudo make clean
 sudo apt-get update
 sudo apt-get -y upgrade
 cd `pwd`/depends
-mkdir SDKs
+sudo mkdir SDKs
 cd SDKs
-wget -c https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.11.sdk.tar.xz
-tar -xf MacOSX10.11.sdk.tar.xz
+sudo wget -c https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.11.sdk.tar.xz
+sudo tar -xf MacOSX10.11.sdk.tar.xz
 cd ..
 sudo make -j2 HOST=x86_64-apple-darwin14
 cd ..
 sudo ./autogen.sh
-mkdir db4
-wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-tar -xzvf db-4.8.30.NC.tar.gz
+sudo mkdir db4
+sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+sudo tar -xzvf db-4.8.30.NC.tar.gz
 cd `pwd`/db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
+sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
 sudo make install
 cd ../../
 sudo ./autogen.sh
-./configure --prefix=`pwd`/depends/x86_64-apple-darwin14
+sudo ./configure --prefix=`pwd`/depends/x86_64-apple-darwin14
 sudo make -j2
 sudo make deploy
 echo "No strip required for this file! DMG file is located in the same folder as this script."
