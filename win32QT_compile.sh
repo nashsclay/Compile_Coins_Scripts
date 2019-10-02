a8d476ee96fb5d07841987e8e419da6c09b2289d
nashsclay <nashsclay@gmail.com>
2019-10-02 01:16:35 -0700

    Update win32QT_compile.sh

--- win32QT_compile.sh
+++ win32QT_compile.sh
@@ -8,14 +8,14 @@ cd `pwd`/depends
 sudo make -j2 HOST=i686-w64-mingw32
 cd ..
 sudo ./autogen.sh
-mkdir db4
-wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-tar -xzvf db-4.8.30.NC.tar.gz
+sudo mkdir db4
+sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+sudo tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
+sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
 sudo make install
 cd ../../
 sudo ./autogen.sh
-./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/i686-w64-mingw32
+sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/i686-w64-mingw32
 sudo make -j2
 echo "Remember to strip the QT file!"
