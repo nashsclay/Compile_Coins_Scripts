cfefe5e353b9478e411a3931581baa376643f859
nashsclay <nashsclay@gmail.com>
2019-10-12 22:26:08 -0700

    Create linux64_ubuntu1904.sh

---
+++ linux64_ubuntu1904.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+sudo make clean
+sudo apt-get update
+sudo apt-get -y upgrade
+cd `pwd`/depends
+sudo make -j2
+cd ..
+sudo ./autogen.sh
+sudo mkdir `pwd`/db4
+sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+sudo tar -xzvf db-4.8.30.NC.tar.gz
+cd `pwd`/db-4.8.30.NC/build_unix/
+sudo wget https://raw.githubusercontent.com/nashsclay/Compile_Coins_Scripts/master/berkeley-db-4.8.30.nc.patch
+sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
+sudo make install
+cd ../../
+sudo ./autogen.sh
+sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu
+sudo make -j2
+echo "Remember to strip the daemon, cli, and tx files!"
