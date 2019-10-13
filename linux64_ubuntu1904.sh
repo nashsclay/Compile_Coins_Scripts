341abf5a36b208e20e4ec0f82db9eafeb88d9bcd
nashsclay <nashsclay@gmail.com>
2019-10-13 16:17:59 -0700

    Update linux64_ubuntu1904.sh

--- linux64_ubuntu1904.sh
+++ linux64_ubuntu1904.sh
@@ -6,16 +6,17 @@ cd `pwd`/depends
 sudo make -j2
 cd ..
 sudo ./autogen.sh
-sudo mkdir `pwd`/db4
-sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-sudo tar -xzvf db-4.8.30.NC.tar.gz
-cd `pwd`/db-4.8.30.NC/build_unix/
-sudo wget https://raw.githubusercontent.com/nashsclay/Compile_Coins_Scripts/master/berkeley-db-4.8.30.nc.patch
-sudo patch ../dbinc/atomic.h > ./berkeley-db-4.8.30.nc.patch
-sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
-sudo make install
-cd ../../
-sudo ./autogen.sh
+# Realized below was already in depends folder so not needed
+# sudo mkdir `pwd`/db4
+# sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+# sudo tar -xzvf db-4.8.30.NC.tar.gz
+# cd `pwd`/db-4.8.30.NC/build_unix/
+# sudo wget https://raw.githubusercontent.com/nashsclay/Compile_Coins_Scripts/master/berkeley-db-4.8.30.nc.patch
+# sudo patch ../dbinc/atomic.h > ./berkeley-db-4.8.30.nc.patch
+# sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
+# sudo make install
+# cd ../../
+# sudo ./autogen.sh
 sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu
 sudo make -j2
 echo "Remember to strip the daemon, cli, and tx files!"
