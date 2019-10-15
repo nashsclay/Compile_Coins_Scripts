3dc892419d4e7c7c976d4a8b4210d709f007a5ea
nashsclay <nashsclay@gmail.com>
2019-10-14 21:34:09 -0700

    Working Master Script, Needs Edits

--- master_build_script.sh
+++ master_build_script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 clear
 
 NC=$'\e[0m'
@@ -18,46 +18,62 @@ echo "1.  ARM32          - Raspberry Pi"
 echo "2.  AARCH64        - Very few devices"
 echo "3.  MacOSX Mojave  - Test"
 echo "4.  MacOSX         - Standard build for Macs"
-echo "5.  Linux 64 bit   - Linux 64 bit / also for Masternodes"
+echo "5.  Linux 64 bit   - Linux 64 bit and for Masternodes"
 echo "6.  Windows 32 bit - Windows 32 bit"
 echo "7.  Windows 64 bit - Windows 64 bit"
+echo "8.  Linux 64 bit 19- Linux 64 bit Ubuntu 19.04"
 echo ""
 read -p "What OS are you building for: " choice
 
 if ! [[ "$choice" =~ ^[0-9]+$ ]]; then
 	echo "Enter only numbers. Exiting..."
 	exit 1
-elif [ "$choice" -gt "$FILECOUNT" ] || [ "$choice" -le "0" ]; then
+elif [ "$choice" -gt "8" ] || [ "$choice" -le "0" ]; then
 	echo "You need to enter a number above. Exiting..."
 	exit 1
 fi
 
 if [ "$choice" -eq "1" ]; then
   FANCY_NAME='ARM32'
+  SCRIPT_NAME='ARM32_compile'
 fi
 if [ "$choice" -eq "2" ]; then
   FANCY_NAME='AARCH64'
+  SCRIPT_NAME='ARM64_compile'
 fi
 if [ "$choice" -eq "3" ]; then
   FANCY_NAME='MacOSX Mojave'
+  SCRIPT_NAME='MacOSX_Mojave_test'
 fi
 if [ "$choice" -eq "4" ]; then
   FANCY_NAME='MACOSX'
+  SCRIPT_NAME='MacOSX_allfiles_QT_compile'
 fi
 if [ "$choice" -eq "5" ]; then
   FANCY_NAME='Linux 64 bit'
+  SCRIPT_NAME='linux64_compile'
 fi
 if [ "$choice" -eq "6" ]; then
   FANCY_NAME='Windows 32 bit'
+  SCRIPT_NAME='win32QT_compile'
 fi
 if [ "$choice" -eq "7" ]; then
   FANCY_NAME='Windows 64 bit'
+  SCRIPT_NAME='win64QT_compile'
+fi
+if [ "$choice" -eq "8" ]; then
+  FANCY_NAME='Linux 64 bit Ubuntu 19.04'
+  SCRIPT_NAME='linux64_ubuntu1904'
 fi
 
+echo ""
 read -p "You are about to build ${RED}$FANCY_NAME${NC}, do you wish to continue? " -n 1 -r
 if [[ ! $next_choice =~ ^[Yy]$ ]]; then
-  #[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
   echo -e "\n"
   clear
   echo "${RED}$FANCY_NAME${NC} Script starting, please wait..."
+  clear
+  sudo wget "https://raw.githubusercontent.com/nashsclay/Compile_Coins_Scripts/master/$SCRIPT_NAME.sh"
+  chmod +x $SCRIPT_NAME.sh
+  sudo ./$SCRIPT_NAME.sh
 fi
