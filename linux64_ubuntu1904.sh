f60e1b27900e4a46545522ce58a465fe9c95d71d
nashsclay <nashsclay@gmail.com>
2019-10-23 13:22:56 -0700

    Update linux64_ubuntu1904.sh

--- linux64_ubuntu1904.sh
+++ linux64_ubuntu1904.sh
@@ -17,6 +17,6 @@ sudo ./autogen.sh
 # sudo make install
 # cd ../../
 # sudo ./autogen.sh
-sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu
+sudo ./configure --prefix=`pwd`/depends/x86_64-pc-linux-gnu
 sudo make -j2
 echo "Remember to strip the daemon, cli, and tx files!"
